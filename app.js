966ed2b6080571010696033a61f2ee4f4a6b846b
Bofi1 <angelborque03github@gmail.com>
2025-04-08 09:54:41 -0700

    comments

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".soju-bottle", {
-    opacity: 1,
-    x: 100,
-    scrolltrigger: {
-        trigger: ".soju-bottle",
-        pin: true,
-    }
-})
 
+// always refresh on top
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+  }
+  window.scrollTo(0, 0);
+
+
+
+// first page animation
 gsap.to("#soju-letters", {
-    x: -100,
+    x: "-100rem",
     opacity: 1,
     scrolltrigger: {
         trigger: "#soju-letters",
+    }
+})
+
+gsap.to(".soju-bottle", {
+    opacity: 1,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".soju-bottle",
+    }
+})
+
+
+
+// scroll with bottle
+gsap.to(".soju-bottle", {
+    x: "600rem",
+    scale: 0.8,
+    rotate: 0,
+    scrollTrigger: {
+        trigger: ".soju-bottle",
+        markers: false,
+        start: "center center",
+        end: "180% center",
         pin: true,
+        scrub: true
+    }
+})
+
+gsap.to("#tittlepin", {
+    scrollTrigger: {
+        trigger: ".soju-bottle",
+        markers: false,
+        start: "52% center",
+        end: "180% center",
+        pin: true
     }
 })
