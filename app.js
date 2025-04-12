f97e29a13b6885a8b1a236736ecf01b4f1f67c52
Bofi1 <angelborque03github@gmail.com>
2025-04-12 03:53:22 -0700

    page 4 done

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 
     // f5 always on top
-window.addEventListener('beforeunload', function () {
-    window.scrollTo(0, 0);
-});
+// window.addEventListener('beforeunload', function () {
+//     window.scrollTo(0, 0);
+// });
 
 
 if (window.innerWidth > 600) {
